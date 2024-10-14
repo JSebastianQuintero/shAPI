ac23ac707e77a2220a7221527d986cf567843dc3
Juan Quintero <quinterojuan0246@gmail.com>
2024-10-14 13:17:13 -0300

    feat marketing salesman and contact models

--- database/models/marketing.py
+++ database/models/marketing.py
@@ -8,16 +8,16 @@ class SalesmanModel(PersonModel):
     __tablename__ = "salesman"
 
     id : Mapped[int] = mapped_column(ForeignKey("person.id"), primary_key=True)
-    # contact_reports : Mapped[List["ContactReportModel"]] = relationship("ContactReportModel", back_populates="salesman")
+    contact_reports : Mapped[List["ContactReportModel"]] = relationship("ContactReportModel", back_populates="salesman")
     total_sales : Mapped[int]
 
-# class ContactReportModel(Base):
-#     __tablename__ = "contact_report"
+class ContactReportModel(Base):
+    __tablename__ = "contact_report"
     
-#     id : Mapped[int] = mapped_column(primary_key=True)
-#     report_date : Mapped[str]
-#     report : Mapped[str]
-#     next_contact : Mapped[str]
-#     salesman_id : Mapped["SalesmanModel"] = mapped_column(ForeignKey("salesman.id"))
-#     salesman: Mapped["SalesmanModel"] = relationship("SalesmanModel", back_populates="contact_reports")
+    id : Mapped[int] = mapped_column(primary_key=True)
+    report_date : Mapped[str]
+    report : Mapped[str]
+    next_contact : Mapped[str]
+    salesman_id : Mapped["SalesmanModel"] = mapped_column(ForeignKey("salesman.id"))
+    salesman: Mapped["SalesmanModel"] = relationship("SalesmanModel", back_populates="contact_reports")
     
