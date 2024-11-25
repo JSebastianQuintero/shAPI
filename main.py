51b73578a19b5cbf39c0644f5017f6244b374d7c
Juan Quintero <quinterojuan0246@gmail.com>
2024-11-25 18:50:52 -0300

    feat: CORS

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 # routes
 from paths.houses import router as houseRouter
 from paths.marketing import router as marketingRouter
 from paths.clients import router as clientRouter
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
 
 app = FastAPI()
 
@@ -13,3 +20,11 @@ app.mount("/housePostImg", StaticFiles(directory="housePostImg"), name="housePos
 app.include_router(houseRouter)
 app.include_router(marketingRouter)
 app.include_router(clientRouter)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
